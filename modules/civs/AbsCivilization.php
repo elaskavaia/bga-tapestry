bf40402db8839c970ed67e32564631743c33a0f9
Victoria La <laskava@gmail.com>
2025-09-23 19:51:21 -0400

    fixed picking traders cube

--- modules/civs/AbsCivilization.php
+++ modules/civs/AbsCivilization.php
@@ -8,7 +8,9 @@ abstract class AbsCivilization {
 
     public function __construct(int $civ, object $game) {
         $this->game = $game;
-        if ($civ == 0) throw new feException("Invalid civ id 0");
+        if ($civ == 0) {
+            throw new feException("Invalid civ id 0");
+        }
         $this->civ = $civ;
     }
 
@@ -21,7 +23,7 @@ abstract class AbsCivilization {
         return $this->game->civilizations[$civ];
     }
 
-    function getRules($field = 'benefit', $def = null) {
+    function getRules($field = "benefit", $def = null) {
         return array_get($this->getCivInfo(), $field, $def);
     }
 
@@ -33,18 +35,18 @@ abstract class AbsCivilization {
         return false;
     }
 
-    function action_activatedAbility($player_id, $ability, $arg, &$state) {
-    }
+    function action_activatedAbility($player_id, $ability, $arg, &$state) {}
 
     function setupCiv(int $player_id, string $start) {
         $civ = $this->civ;
         if (!$start) {
-            $midgame_setup = $this->getRules('midgame_setup', false);
-            if ($midgame_setup)
-                $this->game->benefitCivEntry($civ, $player_id, 'midgame');
+            $midgame_setup = $this->getRules("midgame_setup", false);
+            if ($midgame_setup) {
+                $this->game->benefitCivEntry($civ, $player_id, "midgame");
+            }
         }
         $tokens = $this->game->effect_setupCivTokens($civ, $player_id);
-        return array('tokens' => $tokens, 'outposts' => []);
+        return ["tokens" => $tokens, "outposts" => []];
     }
 
     function hasCiv($player_id) {
@@ -75,10 +77,12 @@ abstract class AbsCivilization {
 
     function getSingleCube(int $player_id = 0) {
         $cubes = $this->game->getStructuresOnCiv($this->civ, BUILDING_CUBE);
-        $cube = array_shift($cubes);
-        if ($cube) return $cube;
+        $cube = array_shift(array_keys($cubes));
+        if ($cube) {
+            return $cube;
+        }
         if ($player_id) {
-            return $this->game->addCube($player_id, 'hand');
+            return $this->game->addCube($player_id, "hand");
         }
         $this->systemAssertTrue("ERR:AbsCivilization:01", $cube);
         return $cube;
@@ -94,20 +98,22 @@ abstract class AbsCivilization {
         return "civ_{$civ}_$slot";
     }
 
-    function placeCivCube($player_id, int $spot, int $cube_id = 0, int $state = null, string $message = '*') {
+    function placeCivCube($player_id, int $spot, int $cube_id = 0, int $state = null, string $message = "*") {
         $civ = $this->civ;
         $civ_token_string = "civ_{$civ}_$spot";
 
         $this->systemAssertTrue("Civ slot occupied $civ_token_string", $this->game->getStructureOnCivSlot($civ, $spot) == null);
 
-        if ($state === null) $state = $this->game->getCurrentEra($player_id);
+        if ($state === null) {
+            $state = $this->game->getCurrentEra($player_id);
+        }
 
         // find free cube
         if ($cube_id === 0) {
-            $cube_id = (int) $this->game->addCube($player_id, 'hand');
+            $cube_id = (int) $this->game->addCube($player_id, "hand");
         }
 
-        if ($message == '*') {
+        if ($message == "*") {
             $message = clienttranslate('${player_name} advances on their civilization mat');
         }
 
@@ -124,42 +130,44 @@ abstract class AbsCivilization {
     function populateSlotChoiceForArgs(array &$data) {
         $civ = $this->civ;
         $game = $this->game;
-        $slots = $this->getRules('slots');
-        $slot_choice = $this->getRules('slot_choice');
-        $data['slots_choice'] = [];
+        $slots = $this->getRules("slots");
+        $slot_choice = $this->getRules("slot_choice");
+        $data["slots_choice"] = [];
         if ($slots) {
             $only = [];
-            if ($slot_choice == 'unoccupied') {
+            if ($slot_choice == "unoccupied") {
                 $token_data = $game->getStructuresOnCiv($civ, BUILDING_CUBE);
                 foreach ($token_data as $token) {
-                    $slot = getPart($token['card_location'], 2);
+                    $slot = getPart($token["card_location"], 2);
                     unset($slots[$slot]);
                 }
             }
-            if ($slot_choice == 'occupied') {
+            if ($slot_choice == "occupied") {
                 $token_data = $game->getStructuresOnCiv($civ, BUILDING_CUBE);
                 foreach ($token_data as $token) {
-                    $slot = getPart($token['card_location'], 2);
+                    $slot = getPart($token["card_location"], 2);
                     $only[$slot] = 1;
                 }
             }
             if ($slot_choice) {
                 foreach ($slots as $i => $info) {
-                    if (count($only) == 0 || isset($only[$i]))
-                        $data['slots_choice'][$i]['benefit'] = $info['benefit'];
+                    if (count($only) == 0 || isset($only[$i])) {
+                        $data["slots_choice"][$i]["benefit"] = $info["benefit"];
+                    }
                 }
-                if (count($slots) > 1)
-                    $data['title'] = clienttranslate('Choose one of these options');
+                if (count($slots) > 1) {
+                    $data["title"] = clienttranslate("Choose one of these options");
+                }
             }
         }
-        $income_trigger = $this->getRules('income_trigger', null);
-        $decline = array_get($income_trigger, 'decline', true);
-        $data['decline'] = $decline;
+        $income_trigger = $this->getRules("income_trigger", null);
+        $decline = array_get($income_trigger, "decline", true);
+        $data["decline"] = $decline;
         return $slots;
     }
 
-    function awardBenefits(int $player_id, int $ben, int $count = 1, string $reason = '') {
-        return true;  // cleanup
+    function awardBenefits(int $player_id, int $ben, int $count = 1, string $reason = "") {
+        return true; // cleanup
     }
 
     function queueEraCivAbility($player_id, $incomeTurn = 0) {
@@ -168,36 +176,48 @@ abstract class AbsCivilization {
             $this->game->benefitCivEntry($civ, $player_id);
             return;
         }
-        if (!$incomeTurn)
+        if (!$incomeTurn) {
             $incomeTurn = $this->game->getCurrentEra($player_id);
-        $income_trigger = array_get_def($this->game->civilizations, $civ, 'income_trigger', null);
-        if (!$income_trigger)
-            return; // no income trigger
-        $from = array_get($income_trigger, 'from', 0);
-        $to = array_get($income_trigger, 'to', 0);
-        if ($to == 0)
+        }
+        $income_trigger = array_get_def($this->game->civilizations, $civ, "income_trigger", null);
+        if (!$income_trigger) {
             return;
+        } // no income trigger
+        $from = array_get($income_trigger, "from", 0);
+        $to = array_get($income_trigger, "to", 0);
+        if ($to == 0) {
+            return;
+        }
         switch ($civ) {
             case CIV_HISTORIANS: // HISTORIANS Discard territory tile to give token (when they gain landmark, you gain the exposed beneftits).
-                $cubes = $this->game->getCollectionFromDB("SELECT card_location FROM structure WHERE card_location_arg='$player_id' AND  card_location LIKE 'civ_{$civ}\\_%'");
-                if (count($cubes) == 0)
+                $cubes = $this->game->getCollectionFromDB(
+                    "SELECT card_location FROM structure WHERE card_location_arg='$player_id' AND  card_location LIKE 'civ_{$civ}\\_%'"
+                );
+                if (count($cubes) == 0) {
                     return;
+                }
                 break;
             case CIV_MILITANTS: // MILITANTS Gain exposed benefits at start of income turns
-                if (in_range($incomeTurn, $from, $to))
+                if (in_range($incomeTurn, $from, $to)) {
                     $this->game->militantBenefits();
+                }
                 return;
             default:
                 break;
         }
-        if (in_range($incomeTurn, $from, $to))
+        if (in_range($incomeTurn, $from, $to)) {
             $this->game->benefitCivEntry($civ, $player_id);
-        else {
+        } else {
             $notactive = clienttranslate('${player_name}: ability of ${civ_name} is not applicable in era ${era}');
-            $this->game->notifyWithName('message', $notactive, [
-                'civ_name' => $this->game->getTokenName(CARD_CIVILIZATION, $civ),
-                'era' => $incomeTurn
-            ], $player_id);
+            $this->game->notifyWithName(
+                "message",
+                $notactive,
+                [
+                    "civ_name" => $this->game->getTokenName(CARD_CIVILIZATION, $civ),
+                    "era" => $incomeTurn,
+                ],
+                $player_id
+            );
         }
     }
 }
